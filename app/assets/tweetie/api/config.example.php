a708a5a703127ad0764ceaf2d0536ac72e1fc5e7
Dan Crack <dan@freethinkingdesign.co.uk>
2014-09-27 19:25:52 +0100

    Added cap and cleaned up

---
+++ app/assets/tweetie/api/config.example.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * Your Twitter App Info
+     * Recreate this file as config.php using the correct details
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'CONSUMER_KEY_HERE');
+    define('CONSUMER_SECRET', 'CONSUMER_SECRET_HERE');
+
+    // User Access Token
+    define('ACCESS_TOKEN', 'ACCESS_TOKEN_HERE');
+    define('ACCESS_SECRET', 'ACCESS_SECRET_HERE');
