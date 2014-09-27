171a0943f027bf84717888400e19cde40b489126
Dan Crack <dan@freethinkingdesign.co.uk>
2014-09-27 15:59:16 +0100

    Twitter feed in

---
+++ app/assets/tweetie/api/config.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'AlyAkaSAbxziCOXjqlYTHBgkN');
+    define('CONSUMER_SECRET', 'QvmMczeCDlCcSHWpYvQSUMS8igXGcaceJ5ImtRq2DeMNnfQ3hg');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '7714582-118PT52mmTbStq0mmxu3PbhusN2lJmiaqLDMjhXyVj');
+    define('ACCESS_SECRET', 'SGungYVv2AAxLQ5VKrGtTUlS1yrEfRj0Ie0Q6E6Uo1M2j');
