4171d5cab538667dfcc1ca2464839da8ea535e8c
Dan Crack <dan@freethinkingdesign.co.uk>
2014-09-30 12:35:34 +0100

    Fix overzealous find and replace.

--- Gruntfile.js
+++ Gruntfile.js
@@ -472,7 +472,7 @@ module.exports = function (grunt) {
         'imagemin',
         'htmlmin',
         'uglify:dist',
-        'str:dist'
+        'modernizr:dist'
     ]);
 
     grunt.registerTask('default', [
